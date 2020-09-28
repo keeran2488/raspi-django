9553f99d544445488ce385dbf8f4445e892fea03
Kiran Chaudhary <chaudharyk456@gmail.com>
2020-09-28 20:53:27 +0545

    turning on led

--- raspi/core/views.py
+++ raspi/core/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+#RASPI IMPORTS
+import RPi.GPIO as GPIO
+import time
+
+#SETTING UP RASPI GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT) 
+
+#RASPI Functions
+
+def raspi_led():
+    for i in range(10):
+        GPIO.output(17, GPIO.HIGH)
+        sleep(0.2)
+        GPIO.output(17, GPIO.LOW)
+        sleep(0.2)
+
+
+def home(request):
+    raspi_led()
+    return HttpResponse("Connected to Raspberry Pi!")
