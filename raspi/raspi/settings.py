c80c34566a579063021b35dc0d9e142c2acfdbab
Kiran Chaudhary <chaudharyk456@gmail.com>
2020-09-28 21:00:04 +0545

    modified allowed host

--- raspi/raspi/settings.py
+++ raspi/raspi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rx*fn%u-9nzy-@95vc1ns6&64qss^@&oxrwgxn8rbix&h2ias6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0',]
+ALLOWED_HOSTS = ['0.0.0.0','192.168.1.21',]
 
 
 # Application definition
